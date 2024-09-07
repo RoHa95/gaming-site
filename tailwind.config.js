50cf5878213fdc330f813b8a8880c5d217a4ec8e
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-09-08 02:25:18 +0330

    add navbar section and slide bar section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/js/*.js"],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans:["vazir"]
+      }
+    },
+  },
+  plugins: [],
+};
