580dcf72fec0c6d8504149e7d2762aac997a2970
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-09-13 20:22:44 +0330

    add dark mode button

--- src/js/gamescript.js
+++ src/js/gamescript.js
@@ -1,1 +1,12 @@
-console.log("hiiii");
+console.log("hiiiipppp");
+//dark button
+const darkBtn = document.querySelectorAll(".darkmode");
+
+console.log(darkBtn);
+
+darkBtn.forEach(item=>{
+    item.addEventListener("click",e=>{
+        console.log("dark");
+        document.documentElement.classList.toggle("dark");
+    })
+})
